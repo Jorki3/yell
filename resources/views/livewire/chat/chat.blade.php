ceabc425f517e11b373f8eda55e65fcef4c7608a
Jorki3 <eduardomttzt01@outlook.com>
2023-09-13 02:24:03 -0600

    Chat prepare

--- resources/views/livewire/chat/chat.blade.php
+++ resources/views/livewire/chat/chat.blade.php
@@ -1,3 +1,22 @@
 <div class="p-4">
-    Chat
+    <div class="w-full flex">
+        <img src="https://picsum.photos/200/200" class="rounded-full h-16" />
+        <h3 class="my-auto ml-2 font-semibold text-gray-800 dark:text-white">Eduardo Trevizo</h3>
+
+        <span
+            class="p-4 my-auto ml-auto rounded-md text-gray-800 dark:text-white hover:bg-slate-200 hover:dark:bg-gray-600 hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+            </svg>
+        </span>
+    </div>
+
+    Body
+
+    <div>
+        <textarea rows="1" class="w-full bg-white dark:bg-gray-800 rounded-lg text-gray-800 dark:text-white"
+            placeholder="Escribe un mensaje"></textarea>
+    </div>
 </div>
--- resources/views/livewire/contacts/contact-list.blade.php
+++ resources/views/livewire/contacts/contact-list.blade.php
@@ -1,12 +1,23 @@
 <div class="p-4 min-h-full space-y-2">
-    <h2 class="text-gray-800 dark:text-white text-lg font-semibold">Conversaciones</h2>
+    <div class="flex">
+        <h2 class="text-gray-800 dark:text-white text-lg font-semibold">Conversaciones</h2>
 
-    <input type="text" class="w-full bg-white dark:bg-gray-800 rounded-lg text-gray-800 dark:text-white "
+        <span
+            class="p-1 ml-auto rounded-md text-gray-800 dark:text-white hover:bg-slate-200 hover:dark:bg-gray-600 hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+            </svg>
+        </span>
+    </div>
+
+    <input type="text" class="w-full bg-white dark:bg-gray-800 rounded-lg text-gray-800 dark:text-white"
         placeholder="Buscan alguna conversación" />
 
     <ul class="list-group">
         <li class="py-2 px-1 rounded-md flex hover:bg-slate-200 hover:dark:bg-gray-600 hover:cursor-pointer">
-            <img src="https://picsum.photos/200/200" class="rounded-full h-16" />
+            <img src="https://picsum.photos/200/200" class="rounded-full h-14" />
             <div class="ml-2">
                 <h4 class="font-semibold text-gray-800 dark:text-white">Eduardo Trevizo</h4>
                 <p class="text-gray-600 dark:text-slate-300">Lorem ipsum dolor, sit amet consectet...</p>
@@ -15,7 +26,7 @@
         </li>
 
         <li class="py-2 px-1 rounded-md flex hover:bg-slate-200 hover:dark:bg-gray-600 hover:cursor-pointer">
-            <img src="https://picsum.photos/200/200" class="rounded-full h-16" />
+            <img src="https://picsum.photos/200/200" class="rounded-full h-14" />
             <div class="ml-2">
                 <h4 class="font-semibold text-gray-800 dark:text-white">Eduardo Trevizo</h4>
                 <p class="text-gray-600 dark:text-slate-300">Lorem ipsum dolor, sit amet consectet...</p>
@@ -24,7 +35,7 @@
         </li>
 
         <li class="py-2 px-1 rounded-md flex hover:bg-slate-200 hover:dark:bg-gray-600 hover:cursor-pointer">
-            <img src="https://picsum.photos/200/200" class="rounded-full h-16" />
+            <img src="https://picsum.photos/200/200" class="rounded-full h-14" />
             <div class="ml-2">
                 <h4 class="font-semibold text-gray-800 dark:text-white">Eduardo Trevizo</h4>
                 <p class="text-gray-600 dark:text-slate-300">Lorem ipsum dolor, sit amet consectet...</p>
@@ -33,7 +44,7 @@
         </li>
 
         <li class="py-2 px-1 rounded-md flex hover:bg-slate-200 hover:dark:bg-gray-600 hover:cursor-pointer">
-            <img src="https://picsum.photos/200/200" class="rounded-full h-16" />
+            <img src="https://picsum.photos/200/200" class="rounded-full h-14" />
             <div class="ml-2">
                 <h4 class="font-semibold text-gray-800 dark:text-white">Eduardo Trevizo</h4>
                 <p class="text-gray-600 dark:text-slate-300">Lorem ipsum dolor, sit amet consectet...</p>
@@ -42,7 +53,7 @@
         </li>
 
         <li class="py-2 px-1 rounded-md flex hover:bg-slate-200 hover:dark:bg-gray-600 hover:cursor-pointer">
-            <img src="https://picsum.photos/200/200" class="rounded-full h-16" />
+            <img src="https://picsum.photos/200/200" class="rounded-full h-14" />
             <div class="ml-2">
                 <h4 class="font-semibold text-gray-800 dark:text-white">Eduardo Trevizo</h4>
                 <p class="text-gray-600 dark:text-slate-300">Lorem ipsum dolor, sit amet consectet...</p>
